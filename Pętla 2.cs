5a39657b2720d926f9956aa839f0ed53f0f194e7
MartaKocyba <116761391+MartaKocyba@users.noreply.github.com>
2022-10-27 22:37:28 +0200

    Add files via upload

---
+++ Pętla 2.cs
@@ -0,0 +1,20 @@
+﻿
+int liczba = 0;
+bool sukces;
+int silnia = 1;
+
+
+do
+{
+    Console.Write("Podaj całkowitą liczbę dodatnią: ");
+    sukces = int.TryParse(Console.ReadLine(), out liczba);
+}
+while ((!sukces)||(liczba<0));
+
+for (int i = 2; i <= liczba; i++)
+{
+    silnia = silnia * i;
+
+}
+Console.WriteLine($"Silnia z {liczba} wynosi {silnia}");
+Console.ReadKey();
